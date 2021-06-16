3309182546b66f6631540a49cf6ac2c024d29e8c
zhao-robert <zhao_yanbo@outlook.com>
2021-06-16 21:26:04 +0800

    自动计算 grid-container 的高度

--- js/preventExpendBottomBlock.js
+++ js/preventExpendBottomBlock.js
@@ -1,9 +1,18 @@
-function cancleBottomBlockTransition(){
+function cancleBottomBlockTransition() {
     let blockNumber = document.getElementsByClassName("theBlocks").length;
+    let blocksIsOdd = false; //如果block数为奇数，则为true
     if (blockNumber % 2 === 0)
         blockNumber = blockNumber - 1;
-    else
+    else {
         blockNumber = (blockNumber + 1) / 2 - 1;
+        blocksIsOdd = true;
+    }
     document.getElementsByClassName("theBlocks")[blockNumber].getElementsByClassName("content-pessage")[0].style.transitionProperty = "none";
     document.getElementsByClassName("theBlocks")[blockNumber].className += " preventHover";
+    if (blocksIsOdd) {
+        document.getElementById("grid-container").style.height = `${600 + (blockNumber - 2) * 455 + (blockNumber - 1) * 355}px`;
+    }
+    else {
+        document.getElementById("grid-container").style.height = `${((blockNumber + 1) / 2) * 455 - 100}px`;
+    }
 }
