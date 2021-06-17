3649cbed06603c273e9116adf60491479e5be6d7
zhao-robert <zhao_yanbo@outlook.com>
2021-06-17 11:32:00 +0800

    修正了单排绝对尺寸最下面一排的块在 hover 时导致的排版错误

--- js/preventExpendBottomBlock.js
+++ js/preventExpendBottomBlock.js
@@ -1,28 +1,28 @@
 function cancleBottomBlockTransition_DoubleColumn() {
-    let blockNumber = document.getElementsByClassName("theBlocks").length;
+    let blockNumberDouble = document.getElementsByClassName("theBlocks").length;
     let blocksIsOdd = false; //如果block数为奇数，则为true
-    if (blockNumber % 2 === 0)
-        blockNumber = blockNumber - 1;
+    if (blockNumberDouble % 2 === 0)
+        blockNumberDouble = blockNumberDouble - 1;
     else {
-        blockNumber = (blockNumber + 1) / 2 - 1;
+        blockNumberDouble = (blockNumberDouble + 1) / 2 - 1;
         blocksIsOdd = true;
     }
-    document.getElementsByClassName("theBlocks")[blockNumber].getElementsByClassName("content-pessage")[0].style.transitionProperty = "none";
-    document.getElementsByClassName("theBlocks")[blockNumber].className += " preventHover";
+    document.getElementsByClassName("theBlocks")[blockNumberDouble].getElementsByClassName("content-pessage")[0].style.transitionProperty = "none";
+    document.getElementsByClassName("theBlocks")[blockNumberDouble].className += " preventHover";
     if (blocksIsOdd) {
-        document.getElementById("grid-container").style.height = `${600 + (blockNumber - 2) * 455 + (blockNumber - 1) * 355}px`;
+        document.getElementById("grid-container").style.height = `${600 + (blockNumberDouble - 3) * 455 + (blockNumberDouble - 1) * 355 + 100}px`;
     }
     else {
-        document.getElementById("grid-container").style.height = `${((blockNumber + 1) / 2) * 455 - 100}px`;
+        document.getElementById("grid-container").style.height = `${((blockNumberDouble + 1) / 2) * 455 - 200}px`;
     }
 }
 
 function cancleBottomBlockTransition_SingleColumn() {
-    let blockNumber = document.getElementsByClassName("theBlocks").length - 1;
-    document.getElementsByClassName("theBlocks")[blockNumber].getElementsByClassName("content-pessage")[0].style.transitionProperty = "none";
-    document.getElementsByClassName("theBlocks")[blockNumber].className += " preventHover";
-    if(blockNumber > 2){
-        document.getElementById("grid-container").style.height = `${955 + (blockNumber - 2) * 455}px`;
+    let blockNumberSingle = document.getElementsByClassName("theBlocks").length - 1;
+    document.getElementsByClassName("theBlocks")[blockNumberSingle].getElementsByClassName("content-pessage")[0].style.transitionProperty = "none";
+    document.getElementsByClassName("theBlocks")[blockNumberSingle].className += " preventHover";
+    if(blockNumberSingle > 2){
+        document.getElementById("grid-container").style.height = `${955 + (blockNumberSingle - 2) * 455}px`;
     }
     else{
         document.getElementById("grid-container").style.height = `955px`;
