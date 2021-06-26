88320abeb3d88e9c5781eede6139b2c088332795
zhao-robert <zhao_yanbo@outlook.com>
2021-06-26 18:36:48 +0800

    修复不正确的高度，现在不会换行

--- js/preventExpendBottomBlock.js
+++ js/preventExpendBottomBlock.js
@@ -1,20 +1,23 @@
 function cancleBottomBlockTransition_DoubleColumn() {
-    let blockNumberDouble = document.getElementsByClassName("theBlocks").length;
+    let blockNumberDouble = document.getElementsByClassName("theBlocks").length; //blockNumberDouble 为 theBlocks 数量
     let blocksIsOdd = false; //如果block数为奇数，则为true
     if (blockNumberDouble % 2 === 0)
-        blockNumberDouble = blockNumberDouble - 1;
+        blockNumberDouble = blockNumberDouble - 1; //如果是偶数，blockNumberDouble 为最后一个 theBlocks 的序数
     else {
-        blockNumberDouble = (blockNumberDouble + 1) / 2 - 1;
+        blockNumberDouble = (blockNumberDouble + 1) / 2 - 1; //如果是奇数，blockNumberDouble 为中间那个 theBlocks 的序数
         blocksIsOdd = true;
     }
     document.getElementsByClassName("theBlocks")[blockNumberDouble].getElementsByClassName("content-pessage")[0].style.transitionProperty = "none";
     document.getElementsByClassName("theBlocks")[blockNumberDouble].className += " preventHover";
     if (blocksIsOdd) {
-        document.getElementById("grid-container").style.height = `${600 + (blockNumberDouble - 3) * 455 + (blockNumberDouble - 1) * 355 + 100}px`;
+        document.getElementById("grid-container").style.height = `${600 + (blockNumberDouble -1) * 355}px`; 
+        //奇数，左边突出，355 为未展开内容块的高度，600为标题与社交链接的最大高度，现在展开时会溢出
     }
     else {
-        document.getElementById("grid-container").style.height = `${((blockNumberDouble + 1) / 2) * 455 - 200}px`;
+        document.getElementById("grid-container").style.height = `${((blockNumberDouble + 1) / 2) * 355}px`; //偶数，右边突出
     }
+    //现在要在展开时更改 grid-container 的 height，加上 100px 以防止溢出
+    $('#grid-container').css('height',parseInt($('#grid-container').css('height').replace("px", ""), '10') + 100 + "px");
 }
 
 function cancleBottomBlockTransition_SingleColumn() {
